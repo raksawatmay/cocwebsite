e91512c75c8ecee2b2bbb063aa424c8295c82a9c
mayraksawt <32117710+mayraksawt@users.noreply.github.com>
2021-05-23 06:29:39 +0700

    edit Auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import '../styles/globals.css';
+import {AuthProvider} from '../AuthFirebase/auth';
 
 function MyApp({ Component, pageProps }) {
   return (
-   
+    <AuthProvider>
       <Component {...pageProps} />
-    
+    </AuthProvider>
   );
 }
 
