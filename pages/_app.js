f26046122651d1d0b594392803f2ac483429dac3
mayraksawt <32117710+mayraksawt@users.noreply.github.com>
2021-05-23 06:07:31 +0700

    test

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,9 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
+   
       <Component {...pageProps} />
-    </AuthProvider>
+    
   );
 }
 
