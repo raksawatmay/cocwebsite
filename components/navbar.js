ddb050487059e4f16b1a1ca414663d70972fc5d9
mayraksawt <32117710+mayraksawt@users.noreply.github.com>
2021-05-23 06:13:51 +0700

    new navbar

--- components/navbar.js
+++ components/navbar.js
@@ -4,6 +4,9 @@ const Navbar = () => (
     <div className={styles.topbar}>
     <ol className={styles.navbar}>
         <li className={styles.text}><a href="/"><b>Home</b></a></li>
+        <li className={styles.text1}><a href="/new"><b>New</b></a></li>
+        <li className={styles.text2}><a href="/about"><b>About</b></a></li>
+        <li className={styles.text3}><a href="/contect"><b>Contect</b></a></li>
         <li className={styles.text4}><a href="/login"><b>Admin</b></a></li>
     </ol>
     </div>
