211ba4119fa20145c0a09e14bd967d08dcc0e1d4
mayraksawt <32117710+mayraksawt@users.noreply.github.com>
2021-06-05 21:06:53 +0700

    add css contact

--- pages/contact.js
+++ pages/contact.js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import Container from "../components/Container";
 import Navbar from "../components/navbar";
 import styles from "../styles/contact.module.css";
+import {BiEnvelope} from "react-icons/bi";
+import {FcPhone, FcGlobe, FcPrint } from "react-icons/fc";
 
 export default function Contact() {
     return (
@@ -13,8 +15,19 @@ export default function Contact() {
                 <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
             </Head>
             <Navbar />
-            <div>
-                <div></div>
+            <div className={styles.container}>
+                <div className={styles.texthead}><h1><ins>CONTACT</ins></h1></div>
+                <div className={styles.text}>
+                    <h2>&nbsp;&nbsp;&nbsp;วิทยาลัยการคอมพิวเตอร์ (COLLEGE OF COMPUTING)</h2>
+                    <p>มหาวิทยาลัยสงขลานครินทร์ วิทยาเขตภูเก็ต 80 หมู่ 1 ถ.วิชิตสงคราม ต.กะทู้ อ.กะทู้ จ.ภูเก็ต 83120</p>
+                    <p>Prince of Songkla University Phuket Campus 80 M.1 Vichitsongkram Road Kathu, Phuket 83120</p>
+                </div> 
+                <div className={styles.context}>  
+                    <p><FcPhone/> โทร 076-276-000 ต่อ 6471 , 076-276-000 ต่อ 6562</p>
+                    <p><BiEnvelope/> coc@phuket.psu.ac.th</p>
+                    <p><FcGlobe/> www.computing.psu.ac.th</p>
+                    <p><FcPrint/> Fax 076-276-046</p>
+                </div> 
             </div>
             <div className={styles.footer}>
                 <h4>© 2021-2022 by Siriluck Raksawat CoE PSU Phuket.</h4>
