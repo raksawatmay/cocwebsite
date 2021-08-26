1a11cf744c5e8666ac088faaf69cbfb86b564e1a
mayraksawt <32117710+mayraksawt@users.noreply.github.com>
2021-08-26 13:25:50 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
       </div>
       <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
       <df-messenger
-        intent="Talkwithcocy"
+        intent="Welcome"
         chat-icon="https://sv1.picz.in.th/images/2021/07/16/2WMFEe.png"
         chat-title="Cocy Bot"
         agent-id="7c9f26d7-dba5-493e-9d07-1c4403d833f7"
